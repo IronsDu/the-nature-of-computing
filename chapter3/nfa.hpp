c30c5429d320960e52afbcc3a8d86140efd60c38
irons.du <irons.du@gmai..com>
2023-12-06 16:03:40 +0800

    implement convert NFA to DFA.

--- chapter3/nfa.hpp
+++ chapter3/nfa.hpp
@@ -64,6 +64,11 @@ public:
         return _acceptStateSet.contains(state);
     }
 
+    const std::unordered_set<State>& getAcceptStateSet() const
+    {
+        return _acceptStateSet;
+    }
+
 private:
     const std::unordered_set<State> _acceptStateSet;
 };
@@ -76,6 +81,16 @@ public:
           _rules(std::move(rules)),
           _acceptStates(std::move(acceptStates))
     {}
+
+    const auto& getTnitialState() const
+    {
+        return _initialState;
+    }
+
+    const AcceptStates& getAcceptStates() const
+    {
+        return _acceptStates;
+    }
 
     bool accept(std::list<InputType> inputs) const
     {
@@ -117,6 +132,53 @@ public:
         return false;
     }
 
+    // 获取状态转移表
+    // 外层map的key作为起始状态，其value表示此状态下接受的非空输入所能达到的状态集合
+    std::map<State, std::map<InputType, std::set<State>>> getTransformRelation() const
+    {
+        std::map<State, std::map<InputType, std::set<State>>> transformMap;
+
+        for (const auto& rule : _rules)
+        {
+            auto& transform = transformMap[rule.startState()];
+            if (!rule.input())
+            {
+                continue;
+            }
+
+            auto& nextStateSet = transform[rule.input().value()];
+            nextStateSet.insert(rule.nextState());
+            auto emptyInputTargetStateSet = getEmptyInputTargetState(rule.nextState());
+            for (const auto& s : emptyInputTargetStateSet)
+            {
+                nextStateSet.insert(s);
+            }
+        }
+
+        return transformMap;
+    }
+
+    // 计算以某状态开始，以空作为输入所能达到的状态集
+    std::set<State> getEmptyInputTargetState(State startState) const
+    {
+        std::set<State> targetStateSet;
+        for (const auto& rule : _rules)
+        {
+            if (!rule.accept(startState, std::nullopt))
+            {
+                continue;
+            }
+
+            targetStateSet.insert(rule.nextState());
+            auto subTargetStateSet = getEmptyInputTargetState(rule.nextState());
+            for (const auto& s : subTargetStateSet)
+            {
+                targetStateSet.insert(s);
+            }
+        }
+        return targetStateSet;
+    }
+
 private:
     // 返回满足当前状态和输入的规则列表
     std::vector<FARule> _matchRules(const State currentState, const std::optional<InputType> input) const
@@ -147,4 +209,80 @@ static std::list<InputType> convertStringToInputs(const std::string& str)
         inputs.push_back(c);
     }
     return inputs;
+}
+
+// 将一个状态组合转换为一个字符串，通常，这个字符串作为构造新的状态机中的新状态
+static std::string convertCombStateToString(std::set<State> stateSet)
+{
+    std::string str;
+    // 必须依靠稳定顺序
+    for (const auto& s : stateSet)
+    {
+        str += s;
+    }
+    return str;
+}
+
+static void convertNFA2DFA(const NFA& nfa)
+{
+    // 获取NFA的状态转移表
+    const auto transformRelation = nfa.getTransformRelation();
+
+    // 新的起始状态
+    State newInitialState;
+    {
+        // 获取NFA的起始状态，以及获取此起始状态在空输入下所能达到的状态
+        std::set<State> newInitialStateSet;
+        auto initialState = nfa.getTnitialState();
+        auto initialStateTarget = nfa.getEmptyInputTargetState(initialState);
+
+        newInitialStateSet.insert(initialState);
+        for (const auto& s : initialStateTarget)
+        {
+            newInitialStateSet.insert(s);
+        }
+        newInitialState = convertCombStateToString(newInitialStateSet);
+    }
+
+    // 从状态转移表中构造状态顺序表，用于后续的状态组合
+    std::vector<State> stateList;
+    for (const auto& [state, _] : transformRelation)
+    {
+        stateList.push_back(state);
+    }
+
+    std::vector<FARule> rules;
+    // 计算状态的排列组合，计算每一个组合状态集在一些输入下将转移到的状态集
+    // 状态集都将转换为字符串，作为新的状态机的状态，并以此构造新的转换规则
+    for (int i = 0; i < stateList.size(); i++)
+    {
+        std::set<State> stateComb;
+        for (int j = i; j < stateList.size(); j++)
+        {
+            // 构造状态组合，作为起始状态集
+            stateComb.insert(stateList[j]);
+
+            // 计算此状态组合在一些输入下所能达到的状态集合
+            std::map<InputType, std::set<State>> combTransform;
+            for (const auto& s : stateComb)
+            {
+                const auto& transform = transformRelation.at(s);
+                for (const auto& [input, stateSet] : transform)
+                {
+                    for (const auto& s : stateSet)
+                    {
+                        combTransform[input].insert(s);
+                    }
+                }
+            }
+
+            // 使用起始状态集何输入以及转移的目标状态集构造新的DFA中的规则
+            auto newState = convertCombStateToString(stateComb);
+            for (const auto& [input, nextStateSet] : combTransform)
+            {
+                auto newNextState = convertCombStateToString(nextStateSet);
+                rules.push_back(FARule(newState, input, newNextState));
+            }
+        }
+    }
 }
